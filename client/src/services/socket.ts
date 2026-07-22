13da1d02c5850846a3d50e7d9d039832f55d3b60
Aymen Shoteri <95427313+AymenS02@users.noreply.github.com>
2026-07-22 19:18:07 -0400

    Update socket.ts

--- client/src/services/socket.ts
+++ client/src/services/socket.ts
@@ -1,11 +1,7 @@
 import { io } from "socket.io-client";
 
-
 export const socket = io(
-    "http://localhost:5000",
-    {
-      autoConnect: true,
-    }
+  import.meta.env.VITE_API_URL
 );
 
 socket.on("connect", () => {
